52939a699c131a2f93368c61063a433459150cf6
sightorld <47320113+sightorld@users.noreply.github.com>
2019-03-13 08:35:11 +0300

    Update TYAP 2.2.py

--- TYAP 2.2.py
+++ TYAP 2.2.py
@@ -1,8 +1,9 @@
 my_string = "Ф;И;О;Возраст;Категория;_Иванов;Иван;Иванович;23 года;Студент 3 курса;_Петров;Семен;Игоревич;22 года;Студент 2 курса"
-my_string = my_string.split(';_')
-for i in range(0,len(my_string)):
-    my_string[i]=my_string[i].split(';') 
-print('\t' + my_string[0][0]+my_string[0][1]+my_string[0][2] + '\t\t\t' + my_string[0][3] + '\t\t' + my_string[0][4])
-for i in range(1,len(my_string)):
-    print(my_string[i][0]+ ' ' + my_string[i][1]+ ' ' + my_string[i][2],'\t\t', my_string[i][3],'\t',my_string[i][4])
+p = my_string.split(';_')
+for i in range(0,len(p)):
+    p[i] = p[i].split(';')
+    if i == 0:
+        print((p[i][0] + p[i][1] + p[i][2]).ljust(25),p[i][3].ljust(8),p[i][4])
+    else:
+        print((p[i][0]+' '+ p[i][1]+' '+p[i][2]).ljust(25),p[i][3].ljust(8),p[i][4])
 
