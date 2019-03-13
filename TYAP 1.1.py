70eb4fdc70d66b46ead3636f8799e9555d809d92
sightorld <47320113+sightorld@users.noreply.github.com>
2019-03-13 08:13:59 +0300

    Update TYAP 1.1.py

--- TYAP 1.1.py
+++ TYAP 1.1.py
@@ -6,6 +6,5 @@ k = int(input("Ввод K: "))
 if b==0 or a==0:
     print("Произошло деление на ноль :^(")
 else:
-    res = (pow(a,2)-pow(b,3)-(pow(c,3)*pow(a,2)))*(b-c+c*(k-(d/pow(b,3))))-pow((k/b-k/a)*c,2)-20000
-    if res<0: res=-res
-    print(res)   
+    print(abs(( (a**2 - b**3 - c**3 * a**2) * (b - c + c * (k-d/b**3)) - ((k/b - k/a)*c))**2 - 20000))
+ 
