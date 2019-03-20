53dc3ade5b0c2122d9c162f2f4f81dcc74f372d3
sightorld <47320113+sightorld@users.noreply.github.com>
2019-03-20 11:09:29 +0300

    Update TYAP 2.4.py

--- TYAP 2.4.py
+++ TYAP 2.4.py
@@ -1,15 +1,9 @@
 n = int(input("Введите число элементов списка (>10): "))
 p = []
 for i in range(0,n):
-    p.append(input('Введите ' + str(i+1) + ' элемент: '))
+    p.append(int(input('Введите ' + str(i+1) + ' элемент: ')))
 print(p)
-i = 0; b = 0
-while i < n - b:
-    if int(p[i]) % 2 == 0:
-        p.remove(p[i])
-        b = b+1
-        i = i - 1
-    i = i+1
-p.append(input('Введите два новых элемента:\n1) '))
-p.append(input('2) '))
+p = [x for x in p if x % 2 != 0]
+p.append(int(input('Введите два новых элемента:\n1) ')))
+p.append(int(input('2) ')))
 print(p)    
